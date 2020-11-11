8729f9cb6e3bff7fefba30ff2f225a04b11835f2
luminisward <luminis@vip.qq.com>
2020-11-11 23:03:03 +0800

    fix bug

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,5 @@
 var stompClient = null;
 
-
 function connect() {
     var socket = new SockJS('/websocket');
     stompClient = Stomp.over(socket);
@@ -8,9 +7,10 @@ function connect() {
         $("#connection-status").text("成功")
         console.log('Connected: ' + frame);
         updateOnlineCount()
-        stompClient.subscribe('/broadcast/waymark', function (message) {
-            showLog(JSON.parse(message.body));
-            sendMessageToPP(message.body.content)
+        stompClient.subscribe('/broadcast/waymark', function (data) {
+            var message = JSON.parse(data.body)
+            showLog(message);
+            sendMessageToPP(message.content)
         });
         stompClient.subscribe('/broadcast/onlineCount', function (message) {
             setOnlineCount(JSON.parse(message.body));
@@ -28,7 +28,11 @@ function setOnlineCount(message) {
 
 function sendMessageToPP(data) {
     var port = $("#ppport").val()
-    $.post("http://localhost:" + port + "/place", data).fail(function (err) { alert( "PP发送失败" ) });
+    fetch("http://localhost:" + port + "/place", {
+        method: "POST",
+        body: data,
+        headers: new Headers({'Content-Type': 'application/json'})
+    }).catch(err => { console.error('Error:', error)  })
 }
 
 function updateOnlineCount() {
